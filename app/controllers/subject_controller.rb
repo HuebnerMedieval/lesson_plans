742ea17de0c84c09d6024bfb69cee7de10d5545e
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-10-26 14:22:55 -0400

    adds routes for subject_controller

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -1,2 +1,18 @@
 class SubjectController < ApplicationController
+    get "/subjects" do
+        if logged_in?
+            erb :"subjects/subjects"
+        else
+            redirect "/"
+        end
+    end
+
+    get "subjects/:id" do
+        if logged_in?
+            @subject = Subject.find_by(id: params[:id])
+            erb :"subjects/show"
+        else
+            redirect "/"
+        end
+    end
 end
