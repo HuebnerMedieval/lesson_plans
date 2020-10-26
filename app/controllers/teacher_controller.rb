0e7880f78d9bb4de50bd79bdb23d1308e293a0e0
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-10-26 13:36:00 -0400

    adds basic routes to teacher_controller.rb

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,2 +1,70 @@
 class TeacherController < ApplicationController
+
+    get "/signup" do
+        #need to create signup view
+        if logged_in?
+            redirect "/teachers/#{current_user.slug}"
+        else
+            erb :"teachers/signup"
+        end
+    end
+
+    post "/teachers/new" do
+        arr = []
+        name = params[:name]
+        arr << name
+        password = params[:password]
+        arr << password
+        subject = params[:subject]
+        arr << subject
+        if Teacher.find_by(name: name)
+            redirect "/login"
+        elsif arr.include?("")
+            redirect "/signup"
+        else
+            teacher = Teacher.new(name: name, password: password)
+            if teacher.authenticate(password)
+                teacher.save
+                session[:user_id] = teacher.id
+                redirect "/teachers/#{teacher.slug}"
+            end
+        end
+    end
+
+
+    get "/login" do
+        if logged_in?
+            redirect "/teachers/#{current_user.slug}"
+        else
+            erb :"teachers/login"
+        end
+    end
+
+    post "/login" do
+        teacher = Teacher.find_by(name: params[:name])
+        if teacher && Teacher.authenticate(params[:password])
+            session[:user_id] = teacher.id
+            redirect "/teachers/#{teacher.slug}"
+        else
+            redirect "/signup"
+        end
+    end
+
+    get "/teachers" do
+        if logged_in?
+            erb :"teachers/teachers"
+        else
+            redirect "/"
+        end
+    end
+
+    get "/teachers/:slug" do
+        if logged_in?
+            @teacher = Teacher.find_by_slug(params[:slug])
+            erb :"teachers/slug"
+        else
+            redirect "/"
+        end
+    end
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,19 @@ class Teacher < ActiveRecord::Base
     has_many :lessons
     belongs_to :subject
     has_secure_password
+
+    def slug
+        if @slug
+            @slug
+        else
+            @slug = self.name.split.join("-")
+        end
+    end
+
+    def self.find_by_slug(slug)
+        Teacher.all.find do |teacher|
+            teacher.slug == slug
+        end
+    end
+
 end
