5eeeb89df07bf3f56ee4634ef800ccd1232f7717
Eli Huebner <elimhuebner@gmail.com>
2021-03-16 15:52:27 -0400

    adds comments to lesson.rb

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,8 @@
 class Lesson < ActiveRecord::Base
+    # sets ActiveRecord association
     belongs_to :teacher
 
+    # getter method to display the associated subject
     def subject
         self.teacher.subject
     end
