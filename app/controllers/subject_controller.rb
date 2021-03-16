076617f58bfd40b440161503c856a93860a7ef20
Eli Huebner <elimhuebner@gmail.com>
2021-03-16 15:38:14 -0400

    adds comments to subject_controller

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -1,7 +1,10 @@
 class SubjectController < ApplicationController
+    # index route for subjects
     get "/subjects" do
         if logged_in?
+            # instance variable to be used by the erb file
             @subjects = []
+            # iterates through all Subjects. Only subjects with lessons are passed to the view
             Subject.all.each do |subject|
                 if !subject.lessons.empty?
                     @subjects << subject
@@ -13,6 +16,7 @@ class SubjectController < ApplicationController
         end
     end
 
+    # subject show route
     get "/subjects/:id" do
         if logged_in?
             @subject = Subject.find_by(id: params[:id])
